88e26b5ad99418b5d7b06b735a61b6e32fa8efca
texane <texane@debian.localdomain>
2010-07-10 09:12:14 +0000

    [update] add mult_matrix3

--- main.c
+++ main.c
@@ -43,6 +43,126 @@ static void mult_matrix
 }
 
 
+/* parallel version */
+
+typedef struct range
+{
+  size_t i;
+  size_t j;
+} range_t;
+
+typedef struct work
+{
+  gsl_matrix* res;
+  gsl_matrix* lhs;
+  gsl_matrix* rhs;
+
+  /* remaining indices to compute */
+  range_t range;
+} work_t;
+
+
+static void res_to_range(gsl_matrix* res, range_t* range)
+{
+  range->i = 0;
+  range->j = res->size1 * res->size2;
+}
+
+
+static void index_to_res
+(const gsl_matrix* res, unsigned int index, unsigned int* i, unsigned int* j)
+{
+  /* flat index into res ij pos */
+
+  *i = index / res->size2;
+  *j = index % res->size2;
+}
+
+
+#if 0 /* ununsed */
+static void range_to_res
+(const gsl_matrix* res, const range_t* range,
+ unsigned int* imin, unsigned int* jmin,
+ unsigned int* imax, unsigned int* jmax)
+{
+  /* flat range into res ij pos */
+
+  index_to_res(res, range->i, imin, jmin);
+  index_to_res(res, range->j, imax, jmax);
+}
+#endif /* unused */
+
+
+static void prepare_par_work
+(work_t* par_work, gsl_matrix* res, gsl_matrix* lhs, gsl_matrix* rhs)
+{
+  par_work->res = res;
+  res_to_range(res, &par_work->range);
+
+  par_work->lhs = lhs;
+  par_work->rhs = rhs;
+}
+
+static void prepare_seq_work(work_t* seq_work, work_t* par_work)
+{
+  seq_work->res = par_work->res;
+  seq_work->lhs = par_work->lhs;
+  seq_work->rhs = par_work->rhs;
+}
+
+static int next_seq_work(work_t* seq_work, work_t* par_work)
+{
+  /* extract the next work */
+
+  if (!(par_work->range.j - par_work->range.i))
+    return -1;
+
+  seq_work->range.i = par_work->range.i;
+  seq_work->range.j = par_work->range.i + 1;
+
+  ++par_work->range.i;
+
+  return 0;
+}
+
+static void mult_matrix3
+(gsl_matrix* res, gsl_matrix* lhs, gsl_matrix* rhs)
+{
+  work_t par_work;
+  work_t seq_work;
+
+  prepare_par_work(&par_work, res, lhs, rhs);
+  prepare_seq_work(&seq_work, &par_work);
+
+  /* extract sequential work */
+  while (next_seq_work(&seq_work, &par_work) != -1)
+  {
+    unsigned int n;
+
+    /* foreach n, compute res[index(n)] */
+    for (n = seq_work.range.i; n < seq_work.range.j; ++n)
+    {
+      double res = 0.f;
+
+      unsigned int i;
+      unsigned int j;
+      unsigned int k;
+
+      index_to_res(seq_work.res, n, &i, &j);
+
+      for (k = 0; k < seq_work.lhs->size2; ++k)
+      {
+	res +=
+	  gsl_matrix_get(seq_work.lhs, i, k) *
+	  gsl_matrix_get(seq_work.rhs, k, j);
+      }
+
+      gsl_matrix_set(seq_work.res, i, j, res);
+    }
+  }
+}
+
+
 static inline void print_double(double value)
 {
   if (value >= 0.f)
@@ -76,11 +196,18 @@ int main(int ac, char** av)
   gsl_matrix* rhs_matrix = create_matrix_with_data((const double*)rhs_data, 2, 2);
   gsl_matrix* res_matrix = gsl_matrix_alloc(2, 2);
 
+  gsl_matrix_set_zero(res_matrix);
   mult_matrix2(res_matrix, lhs_matrix, rhs_matrix);
   print_matrix(res_matrix);
   printf("---\n");
 
+  gsl_matrix_set_zero(res_matrix);
   mult_matrix(res_matrix, lhs_matrix, rhs_matrix);
+  print_matrix(res_matrix);
+  printf("---\n");
+
+  gsl_matrix_set_zero(res_matrix);
+  mult_matrix3(res_matrix, lhs_matrix, rhs_matrix);
   print_matrix(res_matrix);
   printf("---\n");
 
